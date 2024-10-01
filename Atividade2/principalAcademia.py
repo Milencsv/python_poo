1c2e0420cbc47be0fd79eedee3e94633dc5dd1d0
Victor Milen <victormilen12@gmail.com>
2024-10-01 17:34:43 -0300

    Adicionando atividade sobre poo

--- Atividade2/principalAcademia.py
+++ Atividade2/principalAcademia.py
@@ -15,20 +15,3 @@ listaAlunos[0].calcularImc()
 
 listaAlunos[1].exibirDados()
 listaAlunos[1].calcularImc()
-
-# while True:
-#     nome = input("Digite o nome do aluno: ")
-#     idade = int(input(f"Digite a idade de {nome}: "))
-#     peso = float(input(f"Digite o peso de {nome}: "))
-#     altura = float(input(f"Digite  a altura de {nome}: "))
-#     experiencia = input("para adicionar mais um digite (sim) do contrário digite (não): ")
-#     pessoa[nome, idade, peso, altura]= nome, idade, peso,altura
-    
-#     if experiencia == "não":
-#         break
-#     elif experiencia == "sim":
-#         continue
-    
-# pessoa = Aluno(nome, idade, peso, altura)
-# pessoa.exibirDados()
-# pessoa.calcularImc()
