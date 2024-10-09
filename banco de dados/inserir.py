e22f14371ad1498037eb3669395e3f0dc9442105
Victor Milen <victormilen12@gmail.com>
2024-10-09 17:19:55 -0300

    Inserindo os dados no banco

--- banco de dados/inserir.py
+++ banco de dados/inserir.py
@@ -1,11 +1,11 @@
 import sqlite3
 
 #Passo1 - Conexão com o banco de dados
-conexao = sqlite3.connect('departamento.db')
+conexao = sqlite3.connect('escritorio.db')
 
 #Passo 2 - Verificar se a tabela existe ou não
 tabela = """
-CREAT TABLE IF NOT EXISTS  funcionário(
+CREATE TABLE IF NOT EXISTS  funcionario(
     codigo INTEGER PRIMARY KEY  AUTOINCREMENT,
     nome VARCHAR(100),
     salario FLOAT,
@@ -15,7 +15,7 @@ CREAT TABLE IF NOT EXISTS  funcionário(
 #Passo 3 - Acessar o objeto cursor() da biblioteca sqlite3, para manipular dados do banco
 consulta = conexao.cursor()
 
-#Passo 4 -   Executar o comando de criação da tabela
+#Passo 4 - Executar o comando de criação da tabela
 consulta.execute(tabela)
 
 #INSERIR DADOS NA TABELA
@@ -25,7 +25,17 @@ salario = float(input("Informe o salario do funcionário: "))
 cargo = input("Informe o cargo do funcionário: ")
 
 #Passo 6 - criando comando SQL para inserir ps dados da tabela
-sql = "INSERIR INTO  funcionário VALUES(?,?,?,?)"# Colocamos ? no lugar dos dados reais, para evitar possiveis ataques de sql injection. Isso é uma implementação da biblioteca  sqlite3.
+sql = "INSERT INTO  funcionario VALUES(?,?,?,?)"# Colocamos ? no lugar dos dados reais, para evitar possiveis ataques de sql injection. Isso é uma implementação da biblioteca  sqlite3.
 
 #Passo 7 - Organizar os dados que irão subsituir os ? no comando SQL
 campos = (None , nome, salario, cargo)# Criando uma tupla com os dados que irão substituir ?. Ao informar o valor 'None' estamos dizendo que será atribuido o valor padrão do AUTOINCREMENT
+
+#Passo 8 - Executar o comando SQL e substituir '?' pelos campos
+consulta.execute(sql, campos)
+
+#Passo 9 - Gravar os dados no banco
+conexao.commit()
+print(consulta.rowcount ,"linha(s) inserirdas com sucesso")
+
+#Passo 10 - encerrar a conexão
+conexao.close()
